67aeec4254383c1e2a14fa9ab8b2f7dfbf0579cd
Yosh <github@yosh.is>
2023-07-06 14:59:51 +0200

    formatting progress

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 
 use std::{fs, io};
 
-use rustdoc_types::{GenericParamDefKind, ItemEnum};
+use rustdoc_types::{GenericBound, GenericParamDefKind, ItemEnum, TraitBoundModifier, Type};
 
 #[derive(Debug, PartialEq, PartialOrd, Default)]
 struct Crate {
@@ -146,7 +146,17 @@ fn format_generic_params(generics: &rustdoc_types::Generics) -> String {
                 bounds,
                 default,
                 synthetic,
-            } => continue,
+            } => {
+                if *synthetic {
+                    continue;
+                }
+                let bounds = format_generic_bounds(&bounds);
+                let default = match default {
+                    Some(ty) => format!(" = {}", format_type(ty)),
+                    None => String::new(),
+                };
+                out.push(format!("{name}{bounds}{default}"))
+            }
             GenericParamDefKind::Const { type_, default } => match default {
                 Some(default) => out.push(format!("const {name}: {type_:?} = {default}")),
                 None => out.push(format!("const {name}: {type_:?}")),
@@ -159,6 +169,59 @@ fn format_generic_params(generics: &rustdoc_types::Generics) -> String {
     }
 }
 
+fn format_generic_bounds(bounds: &[GenericBound]) -> String {
+    let mut out = vec![];
+    for bound in bounds {
+        match &bound {
+            GenericBound::TraitBound {
+                trait_,
+                generic_params: _, // TODO: support HRTBs
+                modifier,
+            } => {
+                let trait_ = &trait_.name;
+                let modifier = match modifier {
+                    TraitBoundModifier::None => "",
+                    TraitBoundModifier::Maybe => "?",
+                    TraitBoundModifier::MaybeConst => "~const ",
+                };
+                out.push(format!("{modifier}{trait_}"));
+            }
+            GenericBound::Outlives(_) => continue, // TODO: support lifetimes
+        };
+    }
+    match out.len() {
+        0 => String::new(),
+        _ => format!(": {}", out.join(" + ")),
+    }
+}
+
 fn format_where_bounds(generics: rustdoc_types::Generics) -> String {
     todo!()
+}
+
+fn format_type(ty: &Type) -> String {
+    match ty {
+        Type::ResolvedPath(_) => todo!(),
+        Type::DynTrait(_) => todo!(),
+        Type::Generic(generic) => generic.clone(),
+        Type::Primitive(_) => todo!(),
+        Type::FunctionPointer(_) => todo!(),
+        Type::Tuple(_) => todo!(),
+        Type::Slice(_) => todo!(),
+        Type::Array { type_, len } => todo!(),
+        Type::ImplTrait(_) => todo!(),
+        Type::Infer => todo!(),
+        Type::RawPointer { mutable, type_ } => todo!(),
+        Type::BorrowedRef {
+            lifetime: _,
+            mutable,
+            type_,
+        } => todo!(),
+        Type::QualifiedPath {
+            name,
+            args,
+            self_type,
+            trait_,
+        } => todo!(),
+    }
 }
